e76e52ad20705d1ae889ddf62927480e9c4850a9
Matej Kloucek <matej.kloucek@mild.blue>
2023-11-25 00:12:28 +0100

    Unrevisioned

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { MainPage } from "./components/MainPage";
+
+function App() {
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path={"*"} element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { appTheme } from "./theme/theme";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+    <ThemeProvider theme={createTheme(appTheme)}>
+        <CssBaseline>
+            <App />
+        </CssBaseline>
+    </ThemeProvider>
+);
